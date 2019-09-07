4f48808419c353be4ad843f935bd866aa21ecace
阿托 <1397431571@qq.com>
2019-09-07 14:03:57 +0800

    本地工程 添加到  git。。。。。。。。

--- git-01/src/cn/itcast/test/Demo01.java
+++ git-01/src/cn/itcast/test/Demo01.java
@@ -4,9 +4,5 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaa");
     }
-    public void a(){
-        int i = 10;
-        i++;
-        System.out.println(i);
-    }
+
 }
